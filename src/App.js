1f5128d2f500e77b949572bc73fe6bc4a56e66c1
Tareq <tarequl30@gmail.com>
2021-03-15 02:00:55 +0600

    biggest changer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 import './App.css';
 import firebaseConfig from './firebase.config'
 import firebase from "firebase/app";
@@ -62,11 +64,80 @@ const handleSignOut = () => {
     setUser(signOutUser)
     });
   }
+    const handleChange = (event) => {
+      console.log(event.target.value)
+    }
+    
+    // const signUpButton = document.getElementById('signUp');
+    // const signInButton = document.getElementById('signIn');
+    // const container = document.getElementById('container');
+    
+    // signUpButton.addEventListener('click', () => {
+    //   container.classList.add("right-panel-active");
+    // });
+    
+    // signInButton.addEventListener('click', () => {
+    //   container.classList.remove("right-panel-active");
+    // });
   return (
-    <div className="App">
-       {
+    <>
+    <div>
+       {/* {
          user.signedIn ? <button onClick={handleSignOut}>sign out</button> :
-         <button onClick={handleClick}>sign in</button>
+         <button className="form" onClick={handleClick}>sign in</button>
+         }
+      {
+        user.signedIn && <div><p>welcome {user.name}</p>
+        <p>Your Email: {user.email}</p> 
+        <img src={user.photo} alt="" width="50%"/>
+        </div> 
+      } */}
+      <form >
+      <div class="container" id="container">
+      	<div class="form-container sign-up-container">
+	    	<form action="#">
+			<h1>Create Account</h1>
+			<div class="social-container">
+      <FontAwesomeIcon icon={['fab', 'apple']} />
+    <FontAwesomeIcon icon={['fab', 'microsoft']} />
+    <FontAwesomeIcon icon={['fab', 'google']} />
+			</div>
+			<span>or use your email for registration</span>
+			<input type="text" placeholder="Name" />
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<button>Sign Up</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="#">
+			<h1>Sign Up</h1>
+			<div class="social-container">
+      <FontAwesomeIcon icon={['faFacebook', 'facebook']} />
+    <FontAwesomeIcon icon={['fab', 'microsoft']} />
+    <FontAwesomeIcon icon={['fab', 'google']} />
+			</div>
+			<span>use your account</span>
+      <input type="text" placeholder="Your Email " onChange={handleChange} required/>
+         <br/>
+         <input type="password" name="" placeholder="Password"  required/>
+         <br/>
+         <input className="signInBtn" type="submit" value="submit"/>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Welcome Back!</h1>
+				<p>To keep connected with us please login with your personal info</p>
+				<button class="ghost" id="signIn">Sign In</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Hello, Friend!</h1>
+				<p>Enter your personal details and start journey with us</p>
+        {
+         user.signedIn ? <button onClick={handleSignOut}>sign out</button> :
+         <button onClick={handleClick}>sign in with Google</button>
          }
       {
         user.signedIn && <div><p>welcome {user.name}</p>
@@ -74,7 +145,19 @@ const handleSignOut = () => {
         <img src={user.photo} alt="" width="50%"/>
         </div> 
       }
+			</div>
+		</div>
+	</div>
+</div>
+
+        {/* <input type="text" placeholder="ur email" onChange={handleChange} required/>
+         <br/>
+         <input type="password" name="" placeholder="password"  required/>
+         <br/>
+         <input className="form" type="submit" value="submit"/> */}
+      </form>
     </div>
+   </>
   );
 }
 
